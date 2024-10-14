5cfaf2df00e951b3b0c60049666dec62d79d3ea4
Samandar <zafarovsamandar5444@gmail.com>
2024-10-14 14:30:12 +0500

    12345678

---
+++ main.js
@@ -0,0 +1,14 @@
+let matematika= +prompt("matematikadan olgan balingiz");
+let informatika= +prompt("Informatikadan olgan balingiz");
+let onatili= +prompt("Ona tilidan olgan balingiz");
+let adabiyot= +prompt("Adabiyotdan olgan balingiz");
+let jismoniyTarbiya= +prompt("Jismoniy Tarbiyadan olgan balingiz");
+let hammasi=  matematika + informatika + onatili + adabiyot + jismoniyTarbiya;
+
+
+
+
+
+console.log(hammasi);
+alert("ortacha olgan balingiz :" + hammasi/5);
+
